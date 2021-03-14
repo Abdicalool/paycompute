587950efaa7fb042c818b31a7373bf522bd6b142
Abdi Mohamed <cabdirisaq22@gmail.com>
2021-03-14 13:59:51 +0100

    adiing controller and model

---
+++ Paycompute/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Paycompute.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
